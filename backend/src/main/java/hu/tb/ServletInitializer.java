15dc5dbd1aa5c592905321d251aef824b0546d6b
bocz.tivadar@lanoga.hu <bocz.tivadar@lanoga.hu>
2016-09-16 12:40:12 +0200

    #2

--- backend/src/main/java/hu/tb/ServletInitializer.java
+++ backend/src/main/java/hu/tb/ServletInitializer.java
@@ -2,8 +2,6 @@ package hu.tb;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
@@ -12,20 +10,4 @@ public class ServletInitializer extends SpringBootServletInitializer {
 		return application.sources(BackendApplication.class);
 	}
 
-    /**
-     * Created by Tivadar Bocz on 2016.09.02..
-     */
-    @RestController
-    public static class TestController {
-
-        @RequestMapping("/test")
-        public String getUnsecuredContent() {
-            return "It works";
-        }
-
-        @RequestMapping("/test2")
-        public String getSecuredContent() {
-            return "It is secured content!";
-        }
-    }
 }
